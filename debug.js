c68a011de1129ee3a58f79056b663e86ae755031
claudio <crios235@west-mec.org>
2020-02-12 18:20:00 -0700

    i did my best

--- debug.js
+++ debug.js
@@ -1,37 +1,37 @@
 
 //function for test
-function test1(){
+function test(){
     var inPut = "^><^>>>^<^v<v^^vv^><<^<><<vv^<>^<^v>^vv<>v><vv^^<>>^^^v<<vv><<^>^<^v<^>^v><<<v^<v<<<v<<vv<v<^><^>><>v>v^<<v^^<^v<><^>^<<^^^>v>>v^^<v>>^>vv><v>>^>>v^>^v>^<^^v>^>^^v<v>^^<v<>>v^^v><^><^<<>v^<^<^v<v>v^>>>v^v^>^<>^v<^^vv<v>^>^<>^^<vv^<><<v<^<^^>vv<>^>v<^>^v>v^>^v<>^><>><vv<>v^v<><>v^v>>>>v^^>^><^^<v<^><^<v>>^v^v<>v<<<^<<vvvv<<v^vv^>v^^^<^^^<v>>v<^v>>>>>v<^^^^>v<^<><v>>>>>"
     var output = document.getElementById(ouput);
-    
+}
     //Creates and initialized all of the variables for the program
-    var pos = Array ();
+    var pos = Array();
     var x = 0;
-    var y === 0;    
+    var y =0;    
     var ans = 0;
 
     //creates a grid array with one element "0,0"
     var grid = [0, 0];
     //splits everything in the grid array 
-    grid = inpuT.split(" ");
+    grid = input.split(" ");
 
-    pos..push(x + ", " ++ y);
+    pos.push(x + ", " + y);
 
     //checks each grid element for the symbol in it and then moves accordingly 
     for(var i = 0; i < grid.length; i++){
         check = false;
-        if(grid(i) === "^"){
+         if(grid[i] === "^"){
             x++;
-        }else (grid[i] === ">"){
+        }else if (grid[i] === ">"){
             y++;
-        }else if(grid[i] = "v"){
+        }else if(grid[i] === "v"){
             x--;
-        }else if(grid[i] === <){
+        }else if(grid[i] === "<"){
             y--;
         }
         pos.push(x + ", " + y);
     
-
+    }
     //loop that will make sure no position is counted twice 
     loop:
     for(var j = 0; j<pos.length; j++){
@@ -42,14 +42,14 @@ function test1(){
         }
         ans++;
     }
-    output.textContent.anchor.innerHTML = Ans;
+    output.textContent.anchor.innerHTML = ans;
 
 //function for test 2
-test(){
-    var input = "^><^>>>^<^v<v^^vv^><<^<><<vv^<>^<^v>^vv<>v><vv^^<>>^^^v<<vv><<^>^<^v<^>^v><<<v^<v<<<v<<vv<v<^><^>><>v>v^<<v^^<^v<><^>^<<^^^>v>>v^^<v>>^>vv><v>>^>>v^>^v>^<^^v>^>^^v<v>^^<v<>>v^^v><^><^<<>v^<^<^v<v>v^>>>v^v^>^<>^v<^^vv<v>^>^<>^^<vv^<><<v<^<^^>vv<>^>v<^>^v>v^>^v<>^><>><vv<>v^v<><>v^v>>>>v^^>^><^^<v<^><^<v>>^v^v<>v<<<^<<vvvv<<v^vv^>v^^^<^^^<v>>v<^v>>>>>v<^^^^>v<^<><v>>>>>"
+test2()
+    var input = "^><^>>>^<^v<v^^vv^><<^<><<vv^<>^<^v>^vv<>v><vv^^<>>^^^v<<vv><<^>^<^v<^>^v><<<v^<v<<<v<<vv<v<^><^>><>v>v^<<v^^<^v<><^>^<<^^^>v>>v^^<v>>^>vv><v>>^>>v^>^v>^<^^v>^>^^v<v>^^<v<>>v^^v><^><^<<>v^<^<^v<v>v^>>>v^v^>^<>^v<^^vv<v>^>^<>^^<vv^<><<v<^<^^>vv<>^>v<^>^v>v^>^v<>^><>><vv<>v^v<><>v^v>>>>v^^>^><^^<v<^><^<v>>^v^v<>v<<<^<<vvvv<<v^vv^>v^^^<^^^<v>>v<^v>>>>>v<^^^^>v<^<><v>>>>>";
     var output = document.getElementById("output");
-    var poS = Array [];
-    var x1 === 0;
+    var pos = Array();
+    var x1 = 0;
     var y1 = 0;   
     var X2 = 0;
     var y2 = 0;  
@@ -67,33 +67,34 @@ test(){
         if(i % 2 === 0){
             if(grid[i] === "^"){
                 x1++;
-            }else if(grid[i] !=== ">"){
+            }else if(grid[i] === ">"){
                 y1++;
-            }else if(grid(i)) === "v"){
+            }else if(grid[i] === "v"){
                 x1--;
             }else if(grid[i] === "<"){
                 y1--;
             }
             pos.push(x1 + ", " + y1);
-        
+        }
+    }
         //checks all of the odd values
-        else if(i % 2 === 1){
+         if(i % 2 === 1){
             if(grid[i] === "^"){
                 x2++;
             }else if(gRid === ">"){
                 y2++;
-            }elseif(rid[i] === "v"){
+            }else if(rid[i] === "v"){
                 x2--;
             }else if(grid[I] === "<"){
                 y2--;
             }
-            pos push(x2 + ", " + y2);
+            pos.push(x2 + ", " + y2);
         }
-    }
+    
 
     //loop to check repeats
-    loose:
-    for(var j = 0; j<poS.lngth; j++){
+    loop:
+    for(var j = 0; j<pos.length; j++){
         for(var i = j+1; i<pos.length()-1; i++){
             if(pos[i] === pos[j]){
                 continue loop;
@@ -101,4 +102,4 @@ test(){
         }
         ans++;
     }
-    output.document.innerHTMl = ans;
+    output.document.innerHTML = ans;
